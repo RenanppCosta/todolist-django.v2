0098f29b3233e531db8429e76221b771ee6c9c44
Renanppcosta <renanppcosta@gmail.com>
2024-12-12 22:59:59 -0300

    feat: create taks router created

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,6 @@ from tasks import views
 
 urlpatterns = [
 	path("", views.task_list, name="task_list"),
-    path("<int:id>", views.task_view, name="task_view")
+    path("<int:id>", views.task_view, name="task_view"),
+    path("create", views.create_task, name="create_task")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
+from .forms import TaskModelForm
 
 
 def task_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by("-created_at")
 
     return render(request, "tasks/list-tasks.html", {"tasks": tasks})
 
@@ -11,3 +12,16 @@ def task_view(request, id):
     task = get_object_or_404(Task, id=id)
 
     return render(request, "tasks/task.html", {"task": task})
+
+def create_task(request):
+    if request.method == "POST":
+        form = TaskModelForm(request.POST)
+
+        if form.is_valid():
+            task = form.save()
+            task.save()
+            return redirect("/tasks")
+    else:
+        form = TaskModelForm()
+
+    return render(request,"tasks/create-task.html", {"form": form} )
